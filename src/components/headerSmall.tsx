99c28e58ff5da69a527e3a44dd335b6cfb354245
Nick Casey <nicholas.casey@outlook.com>
2020-07-08 20:07:38 -0400

    Removed React-Bootstrap
    While we discuss it's presence in slack - this marks the resolution of issue #2. Barebones react-router is now implemented.

--- src/components/headerSmall.tsx
+++ src/components/headerSmall.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Container, Jumbotron } from 'react-bootstrap';
+// import { Container, Jumbotron } from 'react-bootstrap'; Typescript is having a very unhappy time with 'Container' 
 import './headerSmall.css';
 
 type HeaderSmallProps = {
@@ -9,12 +9,16 @@ type HeaderSmallProps = {
 
 const HeaderSmall = ({ title, subtitle }: HeaderSmallProps) => {
     return (
-        <Jumbotron fluid className="header-small" >
-            <Container>
-                <h1 className="header-small-title" > {title} < span className="header-small-subtitle" > {subtitle} </span></h1>
-            </Container>
-        </Jumbotron>
+        <h1> a small header, placeholder while we get organized.  </h1> 
     )
 }
+
+/*
+<Jumbotron fluid className="header-small" >
+    <Container>
+        <h1 className="header-small-title" > {title} < span className="header-small-subtitle" > {subtitle} </span></h1>
+    </Container>
+</Jumbotron>
+*/
 
 export default HeaderSmall;
