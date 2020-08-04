631ed3970ac55c1ddf612ded5d04ea7e8c6dbf0b
Tejash Patel <tejashpatel3402@gmail.com>
2020-08-04 06:46:07 -0400

    update blog-card component

--- src/components/blog-card.tsx
+++ src/components/blog-card.tsx
@@ -1,23 +1,31 @@
-import * as React from 'react';
+import React from 'react';
 import './blog-card.css';
+import { Link } from 'react-router-dom';
 
 type BlogCardProps = {
     title: string,
     reading_time: number,
-    published_at?: Date,
+    published_at: Date,
     excerpt: string,
-    feature_image: string
+    feature_image: string,
+    slug: String,
+    tags: Array<any>
 }
 
-const BlogCard = ({ title, reading_time, published_at, excerpt, feature_image }: BlogCardProps) => {
+const BlogCard = ({ title, reading_time, published_at, excerpt, feature_image, slug, tags }: BlogCardProps) => {
     return (
         <div className="card mb-2">
-            <img src={feature_image} alt="img" className="card-img-top" />
-            <div className="card-body">
-                <h4>{title}</h4>
-                <p>{excerpt}</p>
-                <p>{reading_time} MIN Read</p>
-            </div>
+            <Link to={"/blog/" + slug}>
+                <img src={feature_image} alt="img" className="card-img-top" />
+                <div className="card-body">
+                    {tags.map((tag: any) => (<span className="tag">{tag.name.toUpperCase()}</span>))}
+                    <h2>{title}</h2>
+                    <p>{excerpt}</p>
+                    <div className="date-reading-time">
+                        <p>{reading_time} MIN Read</p>
+                    </div>
+                </div>
+            </Link>
         </div>
     )
 }
