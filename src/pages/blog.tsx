594eb89d23915722ddb42c9553e8d6eca067d549
Tejash Patel <tejashpatel3402@gmail.com>
2020-08-04 06:46:30 -0400

    update blog page

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -15,7 +15,7 @@ export default class Blog extends React.Component<object> {
   }
 
   componentDidMount() {
-    fetch("https://admin.cuhacking.com/ghost/api/v3/content/posts/?key=4ff22bcd0f6effc7d2df8ee82b")
+    fetch("https://admin.cuhacking.com/ghost/api/v3/content/posts/?key=4ff22bcd0f6effc7d2df8ee82b&include=tags,authors")
       .then(res => res.json())
       .then(
         (result) => {
@@ -25,9 +25,6 @@ export default class Blog extends React.Component<object> {
             posts: result.posts
           });
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
         (error) => {
           this.setState({
             isLoaded: true,
@@ -39,7 +36,6 @@ export default class Blog extends React.Component<object> {
 
   render() {
     const { error, isLoaded, posts }: any = this.state;
-    console.log(posts);
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
@@ -53,7 +49,7 @@ export default class Blog extends React.Component<object> {
               {posts.map((post: any) => (
                 <div className="col-md-4">
                   <BlogCard key={post.id} title={post.title} excerpt={post.excerpt}
-                    feature_image={post.feature_image} reading_time={post.reading_time} />
+                    feature_image={post.feature_image} reading_time={post.reading_time} slug={post.slug} published_at={post.published_at} tags={post.tags} />
                 </div>
               ))}
             </div>
