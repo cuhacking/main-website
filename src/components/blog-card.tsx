769e5539f597ec391a1502b0bc73e6621654d238
Tejash Patel <tejashpatel3402@gmail.com>
2020-08-15 15:11:52 -0400

    more updates

--- src/components/blog-card.tsx
+++ src/components/blog-card.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './blog-card.css';
 import { Link } from 'react-router-dom';
+import FeatureImage from '../assets/default-feature-image.jpg';
 
 type BlogCardProps = {
     title: string,
@@ -9,7 +10,9 @@ type BlogCardProps = {
     excerpt: string,
     feature_image: string,
     slug: String,
-    tags: Array<any>
+    tags: Array<any>,
+    primary_author_name: String,
+    authors: Array<any>
 }
 
 const BlogCard = ({ title, reading_time, published_at, excerpt, feature_image, slug, tags, authors, primary_author_name }: BlogCardProps) => {
@@ -35,14 +38,42 @@ const BlogCard = ({ title, reading_time, published_at, excerpt, feature_image, s
     return (
         <div className="card mb-2">
             <Link to={"/blog/" + slug}>
-                <img src={feature_image} alt="img" className="card-img-top" />
+                <img src={feature_image == null ? FeatureImage : feature_image} alt="img" className="card-img-top" />
                 <div className="card-body">
-                    {tags.map((tag: any) => (<span className="tag">{tag.name.toUpperCase()}</span>))}
+                    {tags.map((tag: any) => (<span className="tag" key="tag.name">{tag.name.toUpperCase()}</span>))}
                     <h2>{title}</h2>
                     <p>{excerpt}</p>
-                    <div className="date-reading-time">
-                        <p>{reading_time} MIN Read</p>
-                    </div>
+                    <section className="post-full-byline-content">
+
+                        <ul className="author-list">
+                            {authors.map((author: any) => (
+                                <li className="author-list-item" key="author.name">
+                                    <div className="author-card">
+                                        <img className="author-profile-image" src={author.profile_image} alt="Ghost" />
+                                        <div className="author-info">
+                                            <div className="bio">
+                                                <h2>{author.name}</h2>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="author-avatar">
+                                        <img className="author-profile-image" src={author.profile_image} alt="Ghost" />
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+
+                        <section className="post-full-byline-meta">
+                            {/* <h4 className="author-name"><a href="/author/ghost/">{primary_author_name}</a></h4> */}
+                            <h4 className="author-name">{primary_author_name}</h4>
+
+                            <div className="byline-meta-content">
+                                <time className="byline-meta-date">{formatted_published_at}</time>
+                                <span className="byline-reading-time"><span className="bull">•</span> {reading_time} min read</span>
+                            </div>
+                        </section>
+
+                    </section>
                 </div>
             </Link>
         </div>
